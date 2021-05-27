4b7978d4dbff99890798bfa72264211a8a86c3c4
Yee Hung <19034609@myrp.edy.sg>
2021-05-27 14:38:59 +0800

    Update

--- app/src/main/java/com/yeehungchong/knowyourfacts/MainActivity.java
+++ app/src/main/java/com/yeehungchong/knowyourfacts/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     //this is a test comemnt: i love u
@@ -18,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.options, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_previous:
+                return true;
+            case R.id.action_random:
+                return true;
+            case R.id.action_next:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
